249248653990919eaf859259b6a013a7df211ee7
Chukwu Daniel <danielchukwu2004@gmail.com>
2025-06-17 13:38:20 -1100

    A simple game in JavaScript for guessing numbers

---
+++ app.js
@@ -0,0 +1,76 @@
+  const hidden = 64;
+  let score = 25;
+  let attempts = 0;
+  let maxAttempts = 5;
+  let highestScore = 0;
+
+  const form = document.getElementById('game-form');
+  const input = document.getElementById('input-number');
+  const feedback = document.getElementById('feedback');
+  const scoreDisplay = document.getElementById('score-display');
+  const highScoreDisplay = document.getElementById('high-score-display');
+  const replayBtn = document.getElementById('replay');
+  const submit = document.getElementById("submit");
+
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    let guess = parseInt(input.value);
+    attempts++;
+    //score -= 5;
+
+    if (isNaN(guess)) {
+      feedback.textContent = "Please enter a valid number.";
+      score -= 5;
+    }
+    else if (guess < 0) {
+      feedback.textContent = "Your number cannot be less than 0.";
+      score -= 5;
+    }
+    else if (guess < 50 || guess > 70) {
+      feedback.textContent = "Your number must be between 50 and 70.";
+      score -= 5;
+    }
+    
+    else if (guess === hidden) {
+      feedback.textContent = "Correct! You won with a score of " + score + ".";
+      feedback.style.color = "green";
+      if (score > highestScore) highestScore = score;
+      highScoreDisplay.textContent = "Highest Score: " + highestScore;
+      replayBtn.style.display = "inline";
+64
+      input.disabled = true;
+      submit.disabled = true;
+    }
+    else {
+      feedback.textContent = "Incorrect! Try again.";
+      feedback.style.color = "red";
+      score -= 5;
+    }
+    scoreDisplay.textContent = "Score: " + score;
+
+    if (attempts >= maxAttempts && guess !== hidden) {
+      feedback.textContent = "Game over! The hidden number was " + hidden + ". Your score is 0.";
+      
+      score = 0;
+      scoreDisplay.textContent = "Score: 0";
+      if (score > highestScore) highestScore = score;
+      highScoreDisplay.textContent = "Highest Score: " + highestScore;
+      replayBtn.style.display = "inline";
+
+      input.disabled = true;
+    }
+    input.value = '';
+  });
+
+  
+  replayBtn.addEventListener('click', function () {
+    score = 25;
+    attempts = 0;
+    scoreDisplay.textContent = "Score: " + score;
+    feedback.textContent = '';
+    replayBtn.style.display = "none";
+    input.value = '';
+    input.disabled = false; 
+    submit.disabled = false;
+  });
